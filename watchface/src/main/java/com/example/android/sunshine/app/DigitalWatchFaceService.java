3f5ea08d3cf0810050efbced70ef5a427a1938b2
Vinny <vin_palumbo@hotmail.com>
2016-04-29 12:46:28 -0400

    Set the same package names for the embedded wearable APK and the phone APK

--- watchface/src/main/java/com/example/android/sunshine/app/DigitalWatchFaceService.java
+++ watchface/src/main/java/com/example/android/sunshine/app/DigitalWatchFaceService.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.vinnypalumbo.watchface;
+package com.example.android.sunshine.app;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
